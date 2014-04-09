bd14a5316ff8112596dff55e7c5051168dea683c
Daniel Gempesaw <gempesaw@gmail.com>
2014-04-08 23:04:54 -0400

    Add ability to put and get HARs

--- lib/Browsermob/Proxy.pm
+++ lib/Browsermob/Proxy.pm
@@ -2,11 +2,14 @@ package Browsermob::Proxy;
 
 # ABSTRACT: Perl client for the proxies created by the Browsermob server
 use Moo;
+use Carp;
 use JSON;
 use Net::HTTP::Spore;
 use Net::HTTP::Spore::Middleware::DefaultParams;
 
 =head1 SYNOPSIS
+
+Standalone:
 
     my $proxy = Browsermob::Proxy->new(
         server_port => 9090
@@ -18,16 +21,32 @@ use Net::HTTP::Spore::Middleware::DefaultParams;
     # create network traffic across your port
     $proxy->har; # returns a HAR as a JSON blob
 
+with L<Browsermob::Server>:
+
+    my $server = Browsermob::Server->new(
+        server_port = 9090
+    );
+    $server->start; # ignore if your server is already running
+
+    my $proxy = $server->create_proxy;
+    $proxy->new_har('proxy from server!');
+
 =head1 DESCRIPTION
 
-An instance of a Browsermob proxy that you can use to capture HARs,
-filter traffic, limit network speeds, etc. This can be used completely
-independently of L<Browsermob::Server> if you want to manage the
-server separately.
+From L<http://bmp.lightbody.net/>: BrowserMob proxy is based on
+technology developed in the Selenium open source project and a
+commercial load testing and monitoring service originally called
+BrowserMob and now part of Neustar.
 
-If you are manually instantiating instances of this class, you must
-specify the C<server_port> so we know where to find the
-browsermob-proxy server.
+It can capture performance data for web apps (via the HAR format), as
+well as manipulate browser behavior and traffic, such as whitelisting
+and blacklisting content, simulating network traffic and latency, and
+rewriting HTTP requests and responses.
+
+This module is a Perl client interface to interact with the server and
+its proxies. It uses L<Net::HTTP::Spore>. You can use
+L<Browsermob::Server> to manage the server itself in addition to using
+this module to handle the proxies.
 
 =cut
 
@@ -41,19 +60,17 @@ Get a list of ports attached to a ProxyServer managed by ProxyManager
 
 Create a new proxy. This method is automatically invoked upon
 instantiation, so you shouldn't have to call it unless you're doing
-something unexpected.
+something unexpected. In fact, if you do call it, things will probably
+get messed up.
 
 =method delete_proxy
 
 Shutdown the proxy and close the port. This is automatically invoked
 when the C<$proxy> goes out of scope, so you shouldn't have to call
-this either.
+this either. In fact, if you do call it, things will probably
+get messed up.
 
     $proxy->delete_proxy;
-
-=method new_har
-
-
 
 =cut
 
@@ -63,25 +80,44 @@ my $spec = {
     version => '0.01',
     methods => {
         get_proxies => {
-            path => '/',
             method => 'GET',
+            path => '/',
             description => 'Get a list of ports attached to ProxyServer instances managed by ProxyManager'
         },
         create => {
-            path => '/:port',
+            method => 'POST',
+            path => '/',
             optional_params => [
                 'port'
             ],
-            method => 'POST',
             description => 'Create a new proxy. Returns a JSON object {"port": your_port} on success"'
         },
         delete_proxy => {
-            path => '/:port',
             method => 'DELETE',
+            path => '/:port',
             required_params => [
                 'port'
             ],
             description => 'Shutdown the proxy and close the port'
+        },
+        create_new_har => {
+            method => 'PUT',
+            path => '/:port/har',
+            optional_params => [
+                'initialPageRef',
+                'captureHeaders',
+                'captureContent',
+                'captureBinaryContent'
+            ],
+            required_params => [
+                'port'
+            ],
+            description => 'creates a new HAR attached to the proxy and returns the HAR content if there was a previous HAR.'
+        },
+        har => {
+            method => 'GET',
+            path => '/:port/har',
+            description => 'returns the JSON/HAR content representing all the HTTP traffic passed through the proxy'
         }
     }
 };
@@ -101,6 +137,14 @@ your own, or let it automatically assign you a port for the proxy.
 has port => (
     is => 'rw',
     lazy => 1,
+    predicate => 'has_port',
+    default => sub { '' }
+);
+
+has mock => (
+    is => 'rw',
+    lazy => 1,
+    predicate => 'has_mock',
     default => sub { '' }
 );
 
@@ -111,10 +155,22 @@ has _spore => (
         my $self = shift;
         my $client = Net::HTTP::Spore->new_from_string(
             to_json($self->_spec),
-            # trace => 2
+            # trace => 1
         );
         $client->enable('Format::JSON');
-        $client->enable('DefaultParams', default_params => { port => $self->port });
+
+        if ($self->has_port) {
+            $client->enable('DefaultParams', default_params => {
+                port => $self->port
+            });
+        }
+
+        if ($self->has_mock) {
+            # The Mock middleware ignores any middleware enabled after
+            # it; make sure to enable everything else first.
+            $client->enable('Mock', tests => $self->mock);
+        }
+
         return $client;
     },
     handles => [keys $spec->{methods}]
@@ -125,16 +181,15 @@ has _spec => (
     lazy => 1,
     builder => sub {
         my $self = shift;
-        $spec->{base_url} = 'http://localhost:' . $self->server_port . '/proxy';
+        $spec->{base_url} = 'http://127.0.0.1:' . $self->server_port . '/proxy';
         return $spec;
     }
 );
 
 =method new
 
-Instantiate a new proxy. C<server_port> is a required argument; if
-you're using C<BrowserMob::Server>, just invoke that class's
-C<create_proxy> method.
+Instantiate a new proxy. C<server_port> is the only required argument
+if you're instantiating this class manually.
 
     my $proxy = $bmp->create_proxy; # invokes new for you
 
@@ -142,17 +197,54 @@ C<create_proxy> method.
 
 =cut
 
-sub BUILD{
+sub BUILD {
     my ($self, $args) = @_;
+    my $res = $self->create;
 
-    eval {
-        $self->port($self->create->body->{port});
-    };
+    unless ($self->has_port) {
+        $self->port($res->body->{port});
+        $self->_spore->enable('DefaultParams', default_params => {
+            port => $self->port
+        });
+    }
 }
 
-sub DESTROY{
+=method new_har
+
+After creating a proxy, C<new_har> creates a new HAR attached to the
+proxy and returns the HAR content if there was a previous one. If no
+argument is passed, the initial page ref will be "Page 1"; you can
+also pass a string to choose your own initial page ref.
+
+    $proxy->new_har;
+    $proxy->new_har('Google');
+
+=cut
+
+sub new_har {
+    my ($self, $initial_page_ref) = @_;
+    my $payload = {};
+
+    croak "You need to create a proxy first!" unless $self->has_port;
+    if (defined $initial_page_ref) {
+        $payload->{initialPageRef} = $initial_page_ref;
+    }
+
+    $self->_spore->create_new_har(payload => $payload);
+}
+
+
+sub DESTROY {
     my $self = shift;
     $self->delete_proxy;
 }
 
 1;
+
+=head1 SEE ALSO
+
+* http://bmp.lightbody.net/
+* https://github.com/lightbody/browsermob-proxy
+* Browsermob::Server
+
+=cut
--- t/Proxy.t
+++ t/Proxy.t
@@ -4,40 +4,127 @@ use strict;
 use warnings;
 use Test::More;
 use JSON;
-use Browsermob::Server;
+use LWP::UserAgent;
+use Browsermob::Proxy;
 
-my $binary = '/opt/browsermob-proxy-2.0-beta-9/bin/browsermob-proxy';
-my $port = 63638;
-plan skip_all => "Skipping server tests; no binary found" unless -f $binary;
+my $server_port = 63638;
+my $port = 9091;
 
-my $bmp = Browsermob::Server->new(
-    path => $binary,
-    port => $port
-);
-$bmp->start;
-
-PROXY_PORT: {
+SPECIFY_PORT: {
     my $proxy = Browsermob::Proxy->new(
-        server_port => $port
+        server_port => $server_port,
+        port => $port,
+        mock => generate_mock_server()
+    );
+
+    ok(defined $proxy->port, 'created a proxy with our own port');
+    if ($proxy->has_mock) {
+        cmp_ok($proxy->port, 'eq', $port, 'on the default port');
+    }
+
+    my $res = $proxy->has_mock ? $proxy->delete_proxy : { body => ""};
+    ok($res->{body} eq '', 'proxy deletes itself');
+    # ok($delete_count eq 1, 'proxy deletes self when taken out of scope');
+}
+
+NO_PORT: {
+    my $proxy = Browsermob::Proxy->new(
+        server_port => $port,
+        mock => generate_mock_server()
     );
 
     isa_ok($proxy, 'Browsermob::Proxy');
     ok(defined $proxy->port, 'Our new proxy has its own port!');
-
-    my $choose = $bmp->create_proxy(port => 9092);
-    cmp_ok($choose->port, '==', 9092, 'We can pick our own ports!');
 }
-
-my $proxy_list = $bmp->get_proxies->{proxyList};
-ok(scalar @$proxy_list eq 0, 'Proxies automatically delete themselves');
 
 HAR: {
-    my $proxy = Browsermob::Proxy->new(
-        server_port => $port
-    );
+  CREATE_UNNAMED: {
+        my $har_server = generate_mock_server();
 
-    $proxy->new_har('Google');
+        $har_server->{'/proxy/' . $port . '/har'} = sub {
+            my $req = shift;
+            ok($req->method eq 'PUT', 'creating a har uses PUT');
+            ok($req->body eq '{}', 'unnamed har body is empty');
+            $req->new_response(200, ['Content-Type' => 'application/json'], "");
+        };
+
+        my $proxy = Browsermob::Proxy->new(
+            server_port => $server_port,
+            port => $port,
+            mock => $har_server
+        );
+
+        my $res = $proxy->new_har;
+    }
+
+  CREATE_NAMED: {
+        my $har_server = generate_mock_server();
+
+        $har_server->{'/proxy/' . $port . '/har'} = sub {
+            my $req = shift;
+            ok($req->method eq 'PUT', 'creating a named har uses PUT');
+            ok($req->body eq '{"initialPageRef":"Google"}', 'body of new har has name');
+            $req->new_response(200, ['Content-Type' => 'application/json'], "");
+        };
+
+        my $proxy = Browsermob::Proxy->new(
+            server_port => $server_port,
+            port => $port,
+            mock => $har_server
+        );
+
+        my $res = $proxy->new_har('Google');
+        ok($res->body eq "", 'got the expected response back when creating a named har');
+    }
+
+  RETRIEVE: {
+        my $har_server = generate_mock_server();
+        $har_server->{'/proxy/' . $port . '/har'} = sub {
+            my $req = shift;
+            ok($req->method eq 'GET', 'retrieving a har via GET');
+            $req->new_response(200, ['Content-Type' => 'application/json'], '{"sample": "har"}');
+        };
+
+        my $proxy = Browsermob::Proxy->new(
+            server_port => $server_port,
+            port => $port,
+            mock => $har_server
+        );
+
+        my $res = $proxy->har;
+        isa_ok($res->body, 'HASH', 'the retrieved har is a hash');
+    }
 }
 
-$bmp->stop;
+sub generate_mock_server {
+    my $mock_port = shift || $port;
+
+    return {
+        '/proxy/' => sub {
+            my $req = shift;
+            if ($req->method eq 'POST') {
+                use Data::Dumper; use DDP;
+                my $res = {
+                    port => $mock_port
+                };
+                return $req->new_response(200, ['Content-Type' => 'application/json'], to_json($res));
+            }
+        },
+
+        '/proxy/' . $mock_port => sub {
+            my ($req) = @_;
+
+            my %params;
+            eval {
+                %params = @{ $req->get_from_env("spore.params") };
+            };
+
+            if ($req->method eq 'DELETE') {
+                die unless $params{port};
+                return $req->new_response(200, ['Content-Type' => 'application/json'], "");
+            }
+        }
+    }
+}
+
 done_testing;
